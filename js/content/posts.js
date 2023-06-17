a2d74a53b8101946aec0a48235e5d1596bcf3b7f
Daniel Sierra <daniel.sierra@taxfix.de>
2023-06-17 20:21:04 +0200

    new project structure

---
+++ js/content/posts.js
@@ -0,0 +1,21 @@
+// Define an array of blog posts
+export const blogPosts = [
+    {
+        title: "First Blog Post",
+        content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus...",
+        link: "post.html",
+    },
+    {
+        title: "Second Blog Post",
+        content:
+            "Aliquam a risus quis ex cursus malesuada a eget odio. Etiam ac...",
+        link: "post.html",
+    },
+    {
+        title: "Third Blog Post",
+        content:
+            "Maecenas vitae purus vel urna elementum cursus in eu risus...",
+        link: "post.html",
+    },
+];
---
+++ js/content/sidebar.js
@@ -0,0 +1,14 @@
+export const categories = [
+    {
+        title: "Category 1",
+        link: "post.html",
+    },
+    {
+        title: "Category 2",
+        link: "post.html",
+    },
+    {
+        title: "Category 3",
+        link: "post.html",
+    },
+];
