bda74049af51ea589b38a804c95c9284c04bef2e
Daniel Sierra <daniel.sierra@taxfix.de>
2023-06-18 22:15:18 +0200

    Some design home page

--- js/content/posts.js
+++ js/content/posts.js
@@ -1,21 +1,191 @@
 // Define an array of blog posts
-export const blogPosts = [
-    {
-        title: "First Blog Post",
-        content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus...",
-        link: "post.html",
-    },
-    {
-        title: "Second Blog Post",
-        content:
-            "Aliquam a risus quis ex cursus malesuada a eget odio. Etiam ac...",
-        link: "post.html",
-    },
-    {
-        title: "Third Blog Post",
-        content:
-            "Maecenas vitae purus vel urna elementum cursus in eu risus...",
-        link: "post.html",
-    },
-];
+// export const blogPosts = [
+//     {
+//         title: "First Blog Post",
+//         content:
+//             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus...",
+//         link: "post.html",
+//     },
+//     {
+//         title: "Second Blog Post",
+//         content:
+//             "Aliquam a risus quis ex cursus malesuada a eget odio. Etiam ac...",
+//         link: "post.html",
+//     },
+//     {
+//         title: "Third Blog Post",
+//         content:
+//             "Maecenas vitae purus vel urna elementum cursus in eu risus...",
+//         link: "post.html",
+//     },
+// ];
+
+let products = [{
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dwdff66087/products/PROR205-4T-3.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "2021 Heart of the Hide R2G 11.75-Inch Infield/Pitcher's Glove",
+    "original_price": "Price reduced from\n\n$299.95\n\nto",
+    "price": "$224.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw5321a376/products/RPPMBH3-130.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Pro Preferred BH3 Maple Wood Bat",
+    "original_price": "$179.95",
+    "price": "N/A"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw4a27af1a/products/BB1V3-40.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "2021 Velo ACP BBCOR -3 Bat",
+    "original_price": "Price reduced from\n\n$379.95\n\nto",
+    "price": "$149.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw672537b3/products/UT1V10-40.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Rawlings 2022 Velo ACP USSSA Bat, -5, -8, -10",
+    "original_price": "Price reduced from\n\n$279.95\n\nto",
+    "price": "$129.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw70281e90/products/KD2PL-40.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Khris Davis Pro Label Wood Bat, Birch Bat",
+    "original_price": "Price reduced from\n\n$159.95\n\nto",
+    "price": "$119.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw94010e22/products/MM8PL-40.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Pro Label Maple Wood Bat",
+    "original_price": "Price reduced from\n\n$159.95\n\nto",
+    "price": "$119.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw43a8eb13/products/OA1PL-40.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Maple Pro Label Wood Bat",
+    "original_price": "Price reduced from\n\n$159.95\n\nto",
+    "price": "$119.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dwcb9938f6/products/BH3PL-40.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "2021 Bryce Harper Pro Label Wood Bat, Maple Bat",
+    "original_price": "Price reduced from\n\n$159.95\n\nto",
+    "price": "$119.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw4a3bd97b/products/PCM30-3.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Player Preferred 33 in Catchers Mitt",
+    "original_price": "Price reduced from\n\n$84.95\n\nto",
+    "price": "$59.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dwba53bb48/products/PFBDCT-3.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Player Preferred 12.5 in First Base Mitt",
+    "original_price": "Price reduced from\n\n$79.95\n\nto",
+    "price": "$59.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw35be9669/products/MA07S-MR-ADJLHB-SR-6.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "2022 Mach Adjust Left Handed Batting Helmet, Junior & Senior",
+    "original_price": "$79.95",
+    "price": "N/A"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw70fd61e5/products/MA07S-MN-5.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "2022 Mach Adjust Right Handed Batting Helmet, Junior & Senior",
+    "original_price": "$79.95",
+    "price": "N/A"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dwddf6ca2c/products/P120BFL-3.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Player Preferred 12 in Infield/Pitcher Glove",
+    "original_price": "Price reduced from\n\n$74.95\n\nto",
+    "price": "$59.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw3f8936fa/products/P125BFL-3.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Player Preferred 12.5 in Infield/Outfield Glove",
+    "original_price": "Price reduced from\n\n$74.95\n\nto",
+    "price": "$59.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dwcce86b12/products/P140BPS-3.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Player Preferred 14 in Outfield Glove",
+    "original_price": "$74.95",
+    "price": "N/A"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dwa9a76ddd/products/MACHEXTR-B-5.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Mach Right Handed Batting Helmet with EXT Flap, 1-Tone & 2-Tone",
+    "original_price": "Price reduced from\n\n$69.95\n\nto",
+    "price": "$59.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw915d5f51/products/R600-S-1.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Players Team Backpack",
+    "original_price": "Price reduced from\n\n$59.95\n\nto",
+    "price": "$44.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw569c52af/products/151T-40.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Ash Youth Wood Bat",
+    "original_price": "Price reduced from\n\n$59.99\n\nto",
+    "price": "$39.99"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw914196bd/products/RCMRD-B-1.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Comrade Backpack",
+    "original_price": "Price reduced from\n\n$49.95\n\nto",
+    "price": "$39.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dwfa950059/products/peirm_pdp_xl.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Adult Maple Performance Grade Wood Bat Blem",
+    "original_price": "Price reduced from\n\n$59.95\n\nto",
+    "price": "$39.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw36b38fd0/products/FORCEJ-S-1.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Rawlings Long Sleeve Quarter-Zip Jacket",
+    "original_price": "$37.95",
+    "price": "N/A"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw88962f62/products/RSGAH-G-1.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Rawlings Laser Cut Vented Snapback Hat",
+    "original_price": "Price reduced from\n\n$24.95\n\nto",
+    "price": "$12.47"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw5f9f451b/products/TBBS11-130.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Big Stick Youth T-Ball Bat, -11",
+    "original_price": "Price reduced from\n\n$39.95\n\nto",
+    "price": "$24.95"
+}, {
+    "image_url": "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dw7b9b6cfd/products/RSGVH-N-1.jpg?sw=450&sh=450&sm=fit&sfrm=png&bgcolor=ebebeb",
+    "name": "Rawlings FlexFit Laser Cut Vented Hat",
+    "original_price": "Price reduced from\n\n$24.95\n\nto",
+    "price": "$12.47"
+}]
+
+function getPrice(product) {
+    if (product.price === "N/A") {
+        if (product.original_price === "N/A") {
+            return "N/A"
+        }
+        return `Original price ${product.original_price}`
+    }
+    if (product.original_price !== "N/A") {
+        return `${product.original_price} ${product.price}`
+    }
+    return `Reduced price ${product.price}`
+}
+
+function mapProductsToBlogPosts(products) {
+    let content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus..."
+    return products.map((product) => {
+        content = getPrice(product)
+        return {
+            "title": product.name,
+            "img": product.image_url,
+            "content":
+                content,
+            "link": "post.html",
+        }
+    })
+}
+
+async function fetchProducts() {
+    console.log("fetching products")
+    fetch('http://localhost:5000/products')
+        .then(response => response.json())
+        .then(data => {
+            // Handle the received data
+            console.log(data); // Print the products data to the console
+            // const mappedProducts = mapProductsToBlogPosts(data)
+            return data
+        })
+        .catch(error => {
+            // Handle any errors that occurred during the request
+            console.error('Error:', error);
+        });
+}
+
+// products = await fetchProducts()
+// console.log(products)
+
+export const blogPosts = mapProductsToBlogPosts(products);
--- js/posts.js
+++ js/posts.js
@@ -2,18 +2,23 @@ import { blogPosts } from "./content/posts.js";
 
 // Function to generate blog post HTML
 function generateBlogPostHTML(post) {
-    return `
-      <div class="post">
-        <h2>${post.title}</h2>
-        <p>${post.content}</p>
-        <a href="${post.link}">Read More</a>
-      </div>
+  return `
+    <div class="post">
+    <div class="post-image">
+      <img src="${post.img}" alt="Post Image">
+    </div>
+    <div class="post-content">
+      <h2>${post.title}</h2>
+      <p>${post.content}</p>
+      <a href="${post.link}">Read More</a>
+    </div>
+  </div>
     `;
 }
 
 // Loop through blogPosts array and generate HTML for each post
 const postContainer = document.getElementById("postContainer");
 blogPosts.forEach((post) => {
-    const postHTML = generateBlogPostHTML(post);
-    postContainer.innerHTML += postHTML;
+  const postHTML = generateBlogPostHTML(post);
+  postContainer.innerHTML += postHTML;
 });
